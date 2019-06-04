956fbc50ee54e7fb7db06da8451bd309287a5298
dallcharm <dallcharm@gmail.com>
2019-06-03 19:15:37 -0500

    add clase10

---
+++ clase10/sample-math.js
@@ -0,0 +1,5 @@
+const customMath = require('./custom-math')
+console.log(customMath.suma(2,3))
+console.log(customMath.resta(2,3))
+console.log(customMath.multiplicacion(2,3))
+console.log(customMath.division(2,3))
